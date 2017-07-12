29c33a8db92654782de5dc76558d6be48c2df888
Brian Staruk <brian@staruk.me>
2017-07-12 14:26:02 -0400

    add es6-promise & whatwg-fetch (polyfills)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
+// promise & fetch polyfills
+require('es6-promise').polyfill();
+require('whatwg-fetch');
+
 // base styles
 require('./styles/app.css');
 
 // components styles
 require.context('./components/', true, /\.css$/);
 
-// components js
+// button component
 const Button = require('./components/button/Button');
 const buttonEl = document.getElementsByClassName('button');
 buttonEl && new Button({el: buttonEl});
