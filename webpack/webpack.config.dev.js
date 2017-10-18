aa70a462cc731d8fcaf235f494477ec44daa7104
Brian Staruk <brian@staruk.me>
2017-10-18 19:55:27 -0400

    webpack build configs: conform to new linting rules

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,63 +1,61 @@
 const webpackMerge = require('webpack-merge');
 const webpackConfigBase = require('./webpack.config.base.js');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const pathsHelper = require('./lib/paths-helper');
 
-module.exports = function () {
-  return webpackMerge(webpackConfigBase(), {
-    output: {
-      publicPath: '/assets'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [
-              'css-loader',
-              {
-                loader: 'postcss-loader',
-                options: {
-                  sourceMap: 'inline',
-                  plugins: function () {
-                    return [
-                      require('postcss-import'),
-                      require('stylelint')(),
-                      require('postcss-reporter')(),
-                      require('postcss-cssnext')({
-                        features: {
-                          autoprefixer: {
-                            grid: false
-                          }
+module.exports = webpackMerge(webpackConfigBase, {
+  output: {
+    publicPath: '/assets'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: 'inline',
+                plugins: function () {
+                  return [
+                    require('postcss-import'),
+                    require('stylelint')(),
+                    require('postcss-reporter')(),
+                    require('postcss-cssnext')({
+                      features: {
+                        autoprefixer: {
+                          grid: false
                         }
-                      }),
-                      require('postcss-nested'),
-                      require('postcss-remove-root'),
-                      require('css-mqpacker')({
-                        sort: true
-                      })
-                    ];
-                  }
+                      }
+                    }),
+                    require('postcss-nested'),
+                    require('postcss-remove-root'),
+                    require('css-mqpacker')({
+                      sort: true
+                    })
+                  ];
                 }
               }
-            ]
-          })
-        }
-      ]
-    },
-    devServer: {
-      contentBase: pathsHelper('static'),
-      port: 8080,
-      watchOptions: {
-        poll: 1000
-      },
-      stats: {
-        children: false
+            }
+          ]
+        })
       }
-    },
-    plugins: [
-      new ExtractTextPlugin('[name].css')
     ]
-  })
-};
+  },
+  devServer: {
+    contentBase: pathsHelper('static'),
+    port: 8080,
+    watchOptions: {
+      poll: 1000
+    },
+    stats: {
+      children: false
+    }
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ]
+});
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -5,62 +5,60 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const pathsHelper = require('./lib/paths-helper');
 
-module.exports = function() {
-  return webpackMerge(webpackConfigBase(), {
-    output: {
-      publicPath: './'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [
-              'css-loader',
-              {
-                loader: 'postcss-loader',
-                options: {
-                  plugins: function () {
-                    return [
-                      require('postcss-import'),
-                      require('stylelint')(),
-                      require('postcss-reporter')(),
-                      require('postcss-cssnext')({
-                        features: {
-                          autoprefixer: {
-                            grid: false
-                          }
+module.exports = webpackMerge(webpackConfigBase, {
+  output: {
+    publicPath: './'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: function () {
+                  return [
+                    require('postcss-import'),
+                    require('stylelint')(),
+                    require('postcss-reporter')(),
+                    require('postcss-cssnext')({
+                      features: {
+                        autoprefixer: {
+                          grid: false
                         }
-                      }),
-                      require('postcss-nested'),
-                      require('postcss-remove-root'),
-                      require('css-mqpacker')({
-                        sort: true
-                      }),
-                      require('cssnano')({
-                        autoprefixer: false,
-                        safe: true
-                      })
-                    ];
-                  }
+                      }
+                    }),
+                    require('postcss-nested'),
+                    require('postcss-remove-root'),
+                    require('css-mqpacker')({
+                      sort: true
+                    }),
+                    require('cssnano')({
+                      autoprefixer: false,
+                      safe: true
+                    })
+                  ];
                 }
               }
-            ]
-          })
-        }
-      ]
-    },
-    plugins: [
-      new ExtractTextPlugin('[name].css'),
-      new CopyWebpackPlugin([
-        {
-          context: pathsHelper('static'),
-          to: '../',
-          from: '**/**'
-        }
-      ]),
-      new CleanWebpackPlugin(['dist'], { root: pathsHelper('base') })
+            }
+          ]
+        })
+      }
     ]
-  })
-};
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css'),
+    new CopyWebpackPlugin([
+      {
+        context: pathsHelper('static'),
+        to: '../',
+        from: '**/**'
+      }
+    ]),
+    new CleanWebpackPlugin(['dist'], { root: pathsHelper('base') })
+  ]
+});
