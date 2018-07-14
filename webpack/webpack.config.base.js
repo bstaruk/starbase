6762f48c6366ad77c74c4a253181ddb72c5da1ed
Brian Staruk <brian@staruk.me>
2018-07-14 14:18:54 -0400

    1.3.0 (#48)
    * first pass at integrating `offline-plugin`
    
    * first pass at asset hashing (just js & css for now)
    
    * dependency updates
    
    * yarn.lock post-merge
    
    * hashing tweaks/buildout
    
    * asset caching readme coverage
    
    * asset caching readme coverage
    
    * asset caching readme coverage
    
    * postcss-extend integration (#42)
    
    * first pass at postcss-extend support
    
    * resolve build warning
    
    * tidying up (#43)
    
    * tidying up font sizes
    
    * no reason for footer to have an id
    
    * tidy up font sizes
    
    * tidy up header component (viva la BEM)
    
    * words of encouragement
    
    * dependency updates
    
    * only use service worker for production build
    
    * further document offline-plugin removal
    
    * further document offline-plugin removal
    
    * further document offline-plugin removal
    
    * further document offline-plugin removal
    
    * version bump (#45)
    
    * dependency updates
    
    * readme updates
    
    * app description update
    
    * readme tweaks
    
    * template tweaks
    
    * incremental version bump
    
    * replace postcss-cssnext w/ postcss-preset-env (#47)
    
    * first pass at replacing postcss-cssnext with postcss-present-env
    
    * verbiage tweaks
    
    * update dependencies
    
    * reorder base config import (after dependencies)
    
    * configs cleanup
    
    * html entity
    
    * html entity
    
    * minor version bump

--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -1,5 +1,5 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   context: path.resolve(__dirname, '../src'),
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,21 +1,23 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OfflinePlugin = require('offline-plugin');
 const path = require('path');
 const webpackMerge = require('webpack-merge');
-const webpackConfigBase = require('./webpack.config.base.js');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const OfflinePlugin = require('offline-plugin');
 
 // postcss plugins
-const postcssImport = require('postcss-import');
-const stylelint = require('stylelint');
-const postcssReporter = require('postcss-reporter');
-const postcssCssnext = require('postcss-cssnext');
-const postcssNested = require('postcss-nested');
-const postcssRemoveRoot = require('postcss-remove-root');
-const postcssResponsiveType = require('postcss-responsive-type');
-const postcssExtend = require('postcss-extend');
 const cssMqpacker = require('css-mqpacker');
 const cssnano = require('cssnano');
+const postcssExtend = require('postcss-extend');
+const postcssImport = require('postcss-import');
+const postcssNested = require('postcss-nested');
+const postcssPresetEnv = require('postcss-preset-env');
+const postcssRemoveRoot = require('postcss-remove-root');
+const postcssReporter = require('postcss-reporter');
+const postcssResponsiveType = require('postcss-responsive-type');
+const stylelint = require('stylelint');
+
+// import base config
+const webpackConfigBase = require('./webpack.config.base.js');
 
 module.exports = webpackMerge(webpackConfigBase, {
   output: {
@@ -31,15 +33,20 @@ module.exports = webpackMerge(webpackConfigBase, {
           {
             loader: 'postcss-loader',
             options: {
+              ident: 'postcss',
               plugins: () => [
                 stylelint(),
                 postcssReporter(),
                 postcssImport(),
                 postcssNested(),
-                postcssCssnext({
+                postcssPresetEnv({
+                  stage: 1,
                   features: {
-                    autoprefixer: {
-                      grid: false
+                    'custom-properties': {
+                      preserve: false
+                    },
+                    'custom-media': {
+                      preserve: false
                     }
                   }
                 }),
@@ -50,8 +57,7 @@ module.exports = webpackMerge(webpackConfigBase, {
                   sort: true
                 }),
                 cssnano({
-                  autoprefixer: false,
-                  safe: true
+                  preset: 'default'
                 })
               ]
             }
