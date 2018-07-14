db314e896d49b370c65a7d1c49848f9c9ddc64a5
Brian Staruk <brian@staruk.me>
2018-07-14 14:07:41 -0400

    replace postcss-cssnext w/ postcss-preset-env (#47)
    * first pass at replacing postcss-cssnext with postcss-present-env
    
    * verbiage tweaks
    
    * update dependencies
    
    * reorder base config import (after dependencies)
    
    * configs cleanup
    
    * html entity
    
    * html entity
    
    * minor version bump

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,18 +1,21 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const webpackMerge = require('webpack-merge');
-const webpackConfigBase = require('./webpack.config.base.js');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // postcss plugins
-const postcssImport = require('postcss-import');
-const stylelint = require('stylelint');
-const postcssReporter = require('postcss-reporter');
-const postcssCssnext = require('postcss-cssnext');
-const postcssNested = require('postcss-nested');
-const postcssRemoveRoot = require('postcss-remove-root');
-const postcssResponsiveType = require('postcss-responsive-type');
-const postcssExtend = require('postcss-extend');
 const cssMqpacker = require('css-mqpacker');
+const cssnano = require('cssnano');
+const postcssExtend = require('postcss-extend');
+const postcssImport = require('postcss-import');
+const postcssNested = require('postcss-nested');
+const postcssPresetEnv = require('postcss-preset-env');
+const postcssRemoveRoot = require('postcss-remove-root');
+const postcssReporter = require('postcss-reporter');
+const postcssResponsiveType = require('postcss-responsive-type');
+const stylelint = require('stylelint');
+
+// import base config
+const webpackConfigBase = require('./webpack.config.base.js');
 
 module.exports = webpackMerge(webpackConfigBase, {
   output: {
@@ -28,16 +31,21 @@ module.exports = webpackMerge(webpackConfigBase, {
           {
             loader: 'postcss-loader',
             options: {
+              ident: 'postcss',
               sourceMap: 'inline',
               plugins: () => [
                 stylelint(),
                 postcssReporter(),
                 postcssImport(),
                 postcssNested(),
-                postcssCssnext({
+                postcssPresetEnv({
+                  stage: 1,
                   features: {
-                    autoprefixer: {
-                      grid: false
+                    'custom-properties': {
+                      preserve: false
+                    },
+                    'custom-media': {
+                      preserve: false
                     }
                   }
                 }),
@@ -46,6 +54,9 @@ module.exports = webpackMerge(webpackConfigBase, {
                 postcssRemoveRoot(),
                 cssMqpacker({
                   sort: true
+                }),
+                cssnano({
+                  preset: 'default'
                 })
               ]
             }
