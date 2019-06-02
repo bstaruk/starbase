6a661a806479402e85922a6334d7312ec66e151d
Brian Staruk <brian@staruk.me>
2019-06-02 11:02:52 -0400

    use imports instead of requires for css

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
 // offline-plugin
 import './lib/offline-plugin';
 
+// application css
+import './css/app.css';
+
 // remove .no-js from html tag
 document.querySelector('html').classList.remove('no-js');
 
 // fetch & promise polyfills (comment-out if not needed)
 // require('es6-promise').polyfill();
 // require('whatwg-fetch');
-
-// ordered css imports
-require('./css/app.css');
-
-// import all other css
-require.context('./css/', true, /\.css$/);
--- src/css/README.md
+++
@@ -1,3 +0,0 @@
-# CSS
-
-Every `.css` file in this directory will be automatically imported into the project (in `/src/app.js`).
