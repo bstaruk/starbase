af1c1571145d1c4e4ac6ea6848ac4970db5ac9cb
Brian Staruk <brian@staruk.me>
2019-06-02 11:30:47 -0400

    removing variables folder, moving app.css

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ import './lib/offline-plugin';
 
 // application css
 import './lib/fonts/fonts.css';
-import './css/app.css';
-
-// remove .no-js from html tag
-document.querySelector('html').classList.remove('no-js');
+import './app.css';
 
 // fetch & promise polyfills (comment-out if not needed)
 // require('es6-promise').polyfill();
 // require('whatwg-fetch');
+
+// remove .no-js from html tag
+document.querySelector('html').classList.remove('no-js');
