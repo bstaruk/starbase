55993b5f4211b7c358ee3af9d592f8863eded32a
Brian Staruk <brian@staruk.net>
2020-08-13 15:56:43 -0400

    comma dangles are back!

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,21 @@ module.exports = {
   extends: 'airbnb-base',
   plugins: ['import'],
   rules: {
-    'comma-dangle': ['error', 'never'],
     'linebreak-style': 0,
     'no-new': 0,
     'no-plusplus': 0,
-    'no-use-before-define': 0
+    'no-use-before-define': 0,
   },
   globals: {
     document: true,
     window: true,
-    location: true
+    location: true,
   },
   settings: {
     'import/resolver': {
       webpack: {
-        config: 'webpack/webpack.config.base.js'
-      }
-    }
-  }
+        config: 'webpack/webpack.config.base.js',
+      },
+    },
+  },
 };
--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -4,80 +4,90 @@ const path = require('path');
 module.exports = {
   context: path.resolve(__dirname, '../src'),
   entry: {
-    app: ['./app.js']
+    app: ['./app.js'],
   },
   output: {
-    path: path.resolve(__dirname, '../dist')
+    path: path.resolve(__dirname, '../dist'),
   },
   resolve: {
     alias: {
       '@css': path.resolve(__dirname, '../src/css'),
       '@lib': path.resolve(__dirname, '../src/lib'),
-      '@src': path.resolve(__dirname, '../src')
+      '@src': path.resolve(__dirname, '../src'),
     },
-    extensions: ['.js', '.json', '.css']
+    extensions: ['.js', '.json', '.css'],
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader']
+        use: ['babel-loader', 'eslint-loader'],
       },
       {
         test: /\.html$/,
-        use: [{
-          loader: 'html-loader',
-          options: {
-            minimize: true
-          }
-        }]
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
       },
       {
         test: /\.(png|jpg|gif)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 8192,
-            name: 'images/[name].[md5:hash:hex:8].[ext]',
-            esModule: false
-          }
-        }]
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'images/[name].[md5:hash:hex:8].[ext]',
+              esModule: false,
+            },
+          },
+        ],
       },
       {
         test: /\.svg$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: 'images/[name].[md5:hash:hex:8].[ext]'
-          }
-        }]
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[md5:hash:hex:8].[ext]',
+            },
+          },
+        ],
       },
       {
         test: /\.(mp4|ogg)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: 'assets/[name].[md5:hash:hex:8].[ext]'
-          }
-        }]
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'assets/[name].[md5:hash:hex:8].[ext]',
+            },
+          },
+        ],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: 'fonts/[name].[md5:hash:hex:8].[ext]'
-          }
-        }]
-      }
-    ]
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'fonts/[name].[md5:hash:hex:8].[ext]',
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../src/templates/index.html'),
       filename: 'index.html',
-      favicon: path.resolve(__dirname, '../src/templates/images/favicon.png')
-    })
-  ]
+      favicon: path.resolve(__dirname, '../src/templates/images/favicon.png'),
+    }),
+  ],
 };
