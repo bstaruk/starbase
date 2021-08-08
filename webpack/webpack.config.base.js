6fc20d5a7a2ca43a91e22e4ab517dc454a3c5312
Brian Staruk <brian@staruk.net>
2021-08-08 18:16:45 -0400

    move babel config into webpack config (too simple for own file)

--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -22,9 +22,14 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.m?js$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
       },
       {
         test: /\.(sa|sc|c)ss$/,
