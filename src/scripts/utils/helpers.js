be7e77bd40f50a3f339b7e7fb11382cdf3b98317
Brian Staruk <brian@staruk.net>
2021-10-30 10:38:47 -0400

    hasEmpty helper cleanup

--- src/scripts/utils/helpers.js
+++ src/scripts/utils/helpers.js
@@ -1,9 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 
 /**
- * Checks an object for null or undefined entries
- * @param {Object} obj Object to check for null or undefined entries
+ * Checks an object for null or undefined values
+ * @param {Object} obj Object to check for null or undefined values
  * @returns bool
  */
 export const hasEmpty = (obj) =>
-  Object.keys(obj).some((key) => obj[key] === undefined || obj[key] === null);
+  Object.values(obj).some((o) => o === undefined || o === null);
