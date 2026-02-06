8d268f9d50f21f2b4f7a459c567f69820f5a84d6
Brian Staruk <brian@staruk.net>
2026-02-05 20:52:29 -0500

    feat: starbase logo init

--- template/src/routes/__root.tsx
+++ template/src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { QueryClient } from '@tanstack/react-query';
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
+import { StarbaseLogo } from 'atoms/StarbaseLogo';
 import { DarkModeToggle } from 'molecules/DarkModeToggle';
 import { Stargazers } from 'molecules/Stargazers';
 
@@ -27,6 +28,9 @@ const ReactQueryDevtools = import.meta.env.PROD
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: () => (
     <>
+      <header className="flex items-center p-4">
+        <StarbaseLogo className="size-12" />
+      </header>
       <Outlet />
       <footer className="fixed bottom-0 inset-x-0 flex items-center justify-center gap-3 p-4">
         <Stargazers />
