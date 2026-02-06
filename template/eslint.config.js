bcbe35754d1f27cb55bb074e51b4aebb6e2652cb
Brian Staruk <brian@staruk.net>
2026-02-06 06:05:30 -0500

    feat(lint): add eslint-plugin-react-x and eslint-plugin-react-dom

--- template/eslint.config.js
+++ template/eslint.config.js
@@ -1,7 +1,9 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import reactDom from 'eslint-plugin-react-dom';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import reactX from 'eslint-plugin-react-x';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
@@ -12,6 +14,8 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      reactX.configs['recommended-typescript'],
+      reactDom.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
